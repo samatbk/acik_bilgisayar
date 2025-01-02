812e6181ea87d8cd4799317e9c4f359b4f3c8038
reuzdev <reuzdev@gmail.com>
2025-01-02 10:01:25 +0300

    feat: load blogs by slug

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Navbar from '$lib/components/Navbar.svelte';
-	import Content, { metadata } from './content.svx';
+	let { data } = $props();
+    const Content = data.content;
+    const metadata = data.metadata;
 </script>
 
 <Navbar />
@@ -11,7 +13,7 @@
 		<img
 			src="https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=700&quality=85&auto=format&fit=max&s=68f6a70c31fdd9fbdafce53e7d9b752c"
 			class="mr-2 h-12 w-12 rounded-full bg-red-800 text-xl"
-			alt="profile-photo"
+			alt="profile"
 		/>
 
 		<p class="text-xl font-semibold">{metadata.author}</p>
